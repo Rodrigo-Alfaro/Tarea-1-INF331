8b217729516760ed3b258d9b62145184d032d056
Rodrigo At <rodrigo.alfaroo@usm.cl>
2025-03-26 14:52:50 -0300

    basic crud, faltan logs, manejo de excepciones

---
+++ Tarea1/src/Inventario.java
@@ -0,0 +1,101 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+public class Inventario {
+
+    private Map<Productos, Integer> inventario;
+
+    Inventario() {
+        this.inventario = new HashMap<>();
+    }
+
+    //Crud Related
+    protected void agregar_producto(Productos producto, int cantidad) {
+        try {
+            inventario.put(producto, cantidad);
+        } catch (Exception e) {
+            System.out.println("Error al agregar el producto" + e.getMessage());
+        }
+    }
+
+    protected void eliminar_cantidad(Productos producto, int cantidad) {
+        try {
+            int cantidad_disponible = inventario.get(producto) - cantidad;
+            inventario.put(producto, cantidad_disponible);
+        } catch (Exception e) {
+            System.out.println("Error al eliminar la cantidad del producto" + e.getMessage());
+        }
+    }
+
+    protected void eliminar_producto(Productos producto){
+        try {
+            inventario.remove(producto);
+        } catch (Exception e) {
+            System.out.println("Error al eliminar el producto" + e.getMessage());
+        }
+    }
+
+    protected void agregar_cantidad(Productos producto, int cantidad){
+        try {
+            int cantidad_disponible = inventario.get(producto) + cantidad;
+            inventario.put(producto, cantidad_disponible);
+        } catch (Exception e) {
+            System.out.println("Error al agregar la cantidad del producto" + e.getMessage());
+        }
+    }
+
+    protected Productos check_producto(String name) {
+        for (Productos producto : inventario.keySet()) {
+            if (producto.getNombre().equals(name)) {
+                System.out.println("Actualmente hay " + inventario.get(producto) + " unidades");
+                return producto;
+            }
+        }
+        System.out.println("Producto no encontrado");
+        return  null;
+    }
+
+    //reporte
+    protected void reporte_inventario(){
+        System.out.println("Inventario actual:");
+        int val = 0;
+        Vector<String> agotados = new Vector<>();
+        for (Map.Entry<Productos, Integer> entry : inventario.entrySet()) {
+            Productos producto = entry.getKey();
+            int cantidad = entry.getValue();
+            System.out.printf("- Producto: %s (Cantidad: %d)%n", producto.getNombre(), cantidad);
+            val += (int) (producto.getPrecio() * cantidad);
+            if (cantidad == 0) agotados.add(producto.getNombre());
+        }
+        System.out.println("Valor Inventario: " + val);
+        System.out.println("Productos agotados: ");
+        for (String producto : agotados) {
+            System.out.println(producto);
+        }
+    }
+
+    //Print related
+    protected void print_categoria(String categoria){
+        System.out.println("Inventario actual:");
+        for (Map.Entry<Productos, Integer> entry : inventario.entrySet()) {
+            Productos producto = entry.getKey();
+            int cantidad = entry.getValue();
+            if(producto.getCategoria().equals(categoria)){
+                System.out.printf("- Producto: %s (Cantidad: %d)%n", producto.getNombre(), cantidad);
+            }
+        }
+    }
+
+    protected void print_Inventario() {
+        System.out.println("Inventario actual:");
+        for (Map.Entry<Productos, Integer> entry : inventario.entrySet()) {
+            Productos producto = entry.getKey();
+            int cantidad = entry.getValue();
+            System.out.printf("- Producto: %s (Cantidad: %d)%n", producto.getNombre(), cantidad);
+        }
+    }
+}
--- Tarea1/src/Main.java
+++ Tarea1/src/Main.java
@@ -1,9 +1,123 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello World");
+
+        //Inicializando cosas necesarias
+        Usuarios usuario = new Usuarios("admin", "123");
+        Inventario inventario = new Inventario();
+        Scanner scanner = new Scanner(System.in);
+        String name = "", pass = "";
+
+        //test products
+        Productos computador = new Productos("Computador", "Computador de escritorio", 10, 100000, "Electronicos");
+        Productos telefono = new Productos("Telefono", "Telefono celular", 10, 10000, "Electronicos");
+        Productos limpieza = new Productos("Limpieza", "Limpieza de casa", 10, 1000, "Hogar");
+        Productos pintura = new Productos("Pintura", "Pintura de casa", 10, 100, "Hogar");
+        inventario.agregar_producto(computador, 10);
+        inventario.agregar_producto(telefono, 100);
+        inventario.agregar_producto(limpieza, 62);
+        inventario.agregar_producto(pintura, 1);
+
+        //Menu
+        //User handling
+        while (!usuario.check_usuario(usuario, name, pass)){
+            System.out.println("""
+               \t Bienvenido al inventario de productos
+               \t Ingresa tu Usuario
+            """);
+            name = scanner.nextLine();
+            System.out.println("\t Ingresa tu password");
+            pass = scanner.nextLine();
+            if(!usuario.check_usuario(usuario, name, pass)){
+                System.out.println("Usuario o password incorrecto, intenta nuevamente");
+            }
+        }
+
+        while (true){
+            System.out.println("""
+                \tBienvenido al inventario de productos, seleccione una opcion:
+                1) Agregar producto al inventario
+                2) Ver inventario
+                3) Actualizar inventario
+                4) Eliminar producto
+                5) Buscar por categoria
+                6) Buscar por producto
+                7) Generar reporte de inventario
+                8) Salir
+                """);
+            int opcion = scanner.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("Ingresa el nombre del producto");
+                    scanner.nextLine();
+                    String nombre = scanner.nextLine();
+                    System.out.println("Ingresa la descripcion del producto");
+                    String descripcion = scanner.nextLine();
+                    System.out.println("Ingresa la cantidad del producto");
+                    int cantidad = scanner.nextInt();
+                    System.out.println("Ingresa el precio del producto");
+                    double precio = scanner.nextDouble();
+                    System.out.println("Ingresa la categoria del producto");
+                    String categoria = scanner.nextLine();
+                    Productos producto = new Productos(nombre, descripcion, cantidad, precio, categoria);
+                    inventario.agregar_producto(producto, cantidad);
+                    System.out.println("Producto agregado");
+                    break;
+                case 2:
+                    inventario.print_Inventario();
+                    break;
+                case 3:
+                    System.out.println("Ingresa el nombre producto que deseas actualizar");
+                    scanner.nextLine();
+                    String producto_update = scanner.nextLine();
+                    Productos current_producto = inventario.check_producto(producto_update);
+                    if (current_producto == null) break;
+                    System.out.println("1) Agregar cantidad\n2) Eliminar cantidad");
+                    switch (scanner.nextInt()){//idk if this gonna work
+                        case 1:
+                            System.out.println("Cantidad a agregar");
+                            int plus = scanner.nextInt();
+                            inventario.agregar_cantidad(current_producto, plus);
+                            System.out.println("Producto actualizado");
+                            break;
+                        case 2:
+                            System.out.println("Cantidad a eliminar");
+                            int minus = scanner.nextInt();
+                            inventario.eliminar_cantidad(current_producto, minus);
+                            System.out.println("Producto actualizado");
+                            break;
+                    }
+                    break;
+                case 4:
+                    System.out.println("Ingresa el nombre del producto que deseas eliminar");
+                    scanner.nextLine();
+                    String producto_delete = scanner.nextLine();
+                    inventario.eliminar_producto(inventario.check_producto(producto_delete));
+                    System.out.println("Producto eliminado");
+                    break;
+                case 5:
+                    System.out.println("Ingresa la categoria del producto que deseas buscar");
+                    scanner.nextLine();
+                    String categoria_search = scanner.nextLine();
+                    inventario.print_categoria(categoria_search);
+                    break;
+                case 6:
+                    System.out.println("Ingresa el nombre del producto que deseas buscar");
+                    scanner.nextLine();
+                    String producto_search = scanner.nextLine();
+                    inventario.check_producto(producto_search);
+                    break;
+                case 7:
+                    inventario.reporte_inventario();
+                    break;
+                case 8:
+                    System.exit(0);
+
+
+            }
+
+        }
+
     }
 }
